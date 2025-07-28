746cce7df691c8ace47d8a7f8b447e6d847e9ebb
Munishw <munishmukesh98@gmail.com>
2025-07-28 11:43:09 +0530

    Dark theme issue resolved

--- src/components/EmployeeTable.jsx
+++ src/components/EmployeeTable.jsx
@@ -175,7 +175,7 @@ const EmployeeTable = () => {
           <button
             key={i}
             onClick={() => paginate(i + 1)}
-            className={`px-3 py-1 border rounded ${currentPage === i + 1 ? "bg-blue-500 text-white" : "bg-white"}`}
+            className={`px-3 py-1 border dark:text-blue-400 rounded ${currentPage === i + 1 ? "bg-blue-500 text-white" : "bg-white"}`}
           >
             {i + 1}
           </button>
